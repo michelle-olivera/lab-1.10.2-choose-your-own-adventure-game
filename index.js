f30b4e86f82e0bbd6a4db82bc590ba3714509e3c
michelle olivera <oliveramichelle8@gmail.com>
2021-09-13 00:25:30 -0400

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-let userName = window.prompt('Please enter your name:')
+let firstAnswer  = window.prompt('Do you head left or right?')
+if(firstAnswer === 'left'){
+    let secondAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+
+} else if(answer === 'right') {
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
+On the other side of him, you see a shiny chest of treasure. Another path would 
+lead you away from the dragon altogether. Which path do you take?`)
+
+}
+
 let userScore = 0;
 
 for(let i = 0; i < questions.length; i++){
@@ -13,22 +25,6 @@ window.alert('Your score is: '+userScore)
 
 let serializedGames = localStorage.getItem('games')
 
-let games;
-if(serializedGames === null){
-    games = [] 
-} else {
-    games = JSON.parse(serializedGames)
-}
-
-let game = {
-    user: userName,
-    score: userScore
-}
-
-games.push(game)
-
-serializedGames = JSON.stringify(games)
-localStorage.setItem('games', serializedGames)
 
 let highestScoringGame = games[0]
 for(let i = 0; i < games.length; i++){
